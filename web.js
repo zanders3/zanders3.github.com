8eaab191153a89f7e8da30397aef2ae63c2a64a8
Alex Parker <alex@alex-macbook.home>
2012-08-31 18:25:55 +0100

    added gokb upload, modified server

--- web.js
+++ web.js
@@ -1,11 +1,6 @@
 var express = require('express'),
     app = express();
-
-app.configure(function(){
-    app.use(express.static(__dirname));
-});
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.use(express.static(__dirname)).listen(port, function(){ 
+	console.log("Listening on " + port); 
 });
