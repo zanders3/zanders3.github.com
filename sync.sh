eaff120fb8e6095c76d8d53efbed185be5e294b2
zanders3 <3zanders@gmail.com>
2012-12-14 20:48:44 +0000

    Remove unused files for static site.

--- sync.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-s3cmd sync --exclude-from .gitignore ./ s3://www.3zanders.co.uk/
--- web.js
+++
@@ -1,6 +0,0 @@
-var express = require('express'),
-    app = express();
-var port = process.env.PORT || 5000;
-app.use(express.static(__dirname)).listen(port, function(){ 
-	console.log("Listening on " + port); 
-});
