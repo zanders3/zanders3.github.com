03a46d7b491f5b045c3950ed1b7bd541828ce09f
zanders3 <3zanders@gmail.com>
2013-01-19 20:34:11 +0000

    OpenGL tutorial using GLWT.

---
+++ blog/blog.js
@@ -0,0 +1,16 @@
+function GetBlog()
+{
+	var blog = [
+		{
+			date: new Date(2012,2,13),
+			link: "opengl-a-tutorial",
+			title: "OpenGL: A Tutorial"
+		},
+		{
+			date: new Date(2012,1,13),
+			link: "new-begginings",
+			title: "New Begginings"
+		}
+	];
+	return blog;
+}
